266178f01d5b347675839b5ee4fc7b0a5e7e84fa
wafflee1 <oleg-saimon92@yandex.ru>
2018-09-16 15:48:23 +0300

    Сделано

--- iliukhin.php
+++ iliukhin.php
@@ -1,1 +1,15 @@
-//Файл Олега
+<!-- //Файл Олега -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        echo "<h1> Файл Олега </h1>";
+    ?>
+</body>
+</html>
